63baab8a6e4fed86946d9b0ee4de1713a14fc036
JadedPigeon <JadedPigeon137@gmail.com>
2025-09-27 20:39:39 -0400

    Read and print messages.txt 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	messages, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+	defer messages.Close()
+
+	byteslice := make([]byte, 8)
+
+	for {
+		n, err := messages.Read(byteslice)
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading file:", err)
+			break
+		}
+
+		if n > 0 {
+			fmt.Printf("read: %s\n", byteslice[:n])
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+
 }
